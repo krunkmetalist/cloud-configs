082f71d4a586bc9c557ea00847a1ce6d3e8e7f76
Nater <nreboiro@companioncorp.com>
2024-05-30 13:51:29 -0600

    bumping crictl version to 1.30.0

--- install_master.sh
+++ install_master.sh
@@ -48,10 +48,14 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 DIR=/etc/apt/keyrings
 
 if [[ ! -e $DIR ]]; then
+    printf "\n\n"
     echo "$DIR did not exist exists, creating it now..." 1>&2
     sudo mkdir -p -m 755 $DIR
+    printf "\n\n"
 elif [[ ! -d $DIR ]]; then
+    printf "\n\n"
     echo "$DIR already exists but is not a directory..." 1>&2
+    printf "\n\n"
 fi
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
@@ -82,7 +86,7 @@ cgroupDriver: systemd' > kubeadm-config.yaml
 #systemctl restart containerd
 
 # ---- install CRICTL ----
-VERSION="v1.22.0"
+VERSION="v1.30.0"
 wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
 sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
 rm -f crictl-$VERSION-linux-amd64.tar.gz

c826c4a60f829faebc3cb042ed29fe399b227539
Nater <nreboiro@companioncorp.com>
2024-05-30 12:51:56 -0600

    ditching canal, enough is enough. apparently calico now does networking and policies. new changes to the master script, now explicitly installing containerd with configuration.

--- install_master.sh
+++ install_master.sh
@@ -1,3 +1,20 @@
+# ---- install containerd CRI ----
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+  overlay
+  br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe
+# Setup required sysctl params, these persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+  net.bridge.bridge-nf-call-iptables  = 1
+  net.ipv4.ip_forward                 = 1
+  net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+
 # ---- install docker ----
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
 
@@ -61,15 +78,27 @@ apiVersion: kubelet.config.k8s.io/v1beta1
 cgroupDriver: systemd' > kubeadm-config.yaml
 
 # ---- containerd config to work with Kubernetes >=1.26 ----
-echo "SystemdCgroup = true" > /etc/containerd/config.toml
+echo "SystemdCgroup = true" > /etc/containerd/config.toml # fix toml!
 systemctl restart containerd
 
+# ---- install CRICTL ----
+VERSION="v1.22.0"
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm -f crictl-$VERSION-linux-amd64.tar.gz
+
 # ---- kubeadm init ----
-echo 'deploying kubernetes (with canal)...'
-kubeadm init --config kubeadm-config.yaml
+echo 'deploying kubernetes...'
+kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p "$HOME"/.kube
+$ sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+$ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-export KUBECONFIG=/etc/kubernetes/admin.conf # if root
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/canal.yaml -O
-kubectl apply -f canal.yaml
+#export KUBECONFIG=/etc/kubernetes/admin.conf # if root
 
+# so, canal was a massive flop. going to try the plain CIDR approach,
+# but if i do that i cant control the k8s version....hmmm...
+#curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/canal.yaml -O
+#kubectl apply -f canal.yaml
 
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml

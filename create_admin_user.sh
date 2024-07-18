04c4c9551b2b1974d3b97f2865036901812b9d89
nate <nate@localhost.localdomain>
2024-07-17 22:27:57 -0600

    adding sudo to create user script

--- create_admin_user.sh
+++ create_admin_user.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "creating user..."
-groupadd gcompadmin
-useradd -g gcompadmin -G admin -s /bin/bash -d /home/compadmin compadmin -p derptest
-mkdir -p /home/compadmin
-cp -r /root/.ssh /home/compadmin/.ssh
-chown -R compadmin:gcompadmin /home/compadmin
+sudo groupadd gcompadmin
+sudo useradd -g gcompadmin -G admin -s /bin/bash -d /home/compadmin compadmin -p derptest
+sudo mkdir -p /home/compadmin
+sudo cp -r /root/.ssh /home/compadmin/.ssh
+sudo chown -R compadmin:gcompadmin /home/compadmin
 echo "gcompadmin ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
